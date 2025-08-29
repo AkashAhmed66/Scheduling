ab8800ed266d71a6c3b53709c7a53a56bd27fe85
AkashAhmed66 <akashahmed662001@gmail.com>
2025-08-29 15:54:49 +0600

    audit docs

--- app/Http/Controllers/AuditDocumentsController.php
+++ app/Http/Controllers/AuditDocumentsController.php
@@ -33,7 +33,7 @@ class AuditDocumentsController extends Controller
             ->with(['children', 'files'])
             ->get();
         
-        return Inertia::render('AuditDocs1', [
+        return Inertia::render('AuditDocs/Index', [
             'rootFolders' => $rootFolders,
             'canManage' => $user->role === 0, // Only roles 0 and 1 can manage
         ]);
